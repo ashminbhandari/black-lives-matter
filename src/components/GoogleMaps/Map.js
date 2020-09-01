11fce9e3b424869fbe4790a2ef2d8beb641a912e
ashminbhandari <abhandar@ramapo.edu>
2020-09-01 02:15:03 -0500

    add google maps with styles, no dependencies

---
+++ src/components/GoogleMaps/Map.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef } from 'react'
+import mapStyles from "./mapStyles";
+
+export default function Map({ options, onMount, className }) {
+    const props = { ref: useRef(), className }
+    const onLoad = () => {
+        const map = new window.google.maps.Map(props.ref.current, options)
+        onMount && onMount(map)
+    }
+
+    useEffect(() => {
+        if (!window.google) {
+            const script = document.createElement(`script`)
+            script.type = `text/javascript`
+            script.src =
+                `https://maps.google.com/maps/api/js?key=` +
+                process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+            const headScript = document.getElementsByTagName(`script`)[0]
+            headScript.parentNode.insertBefore(script, headScript)
+            script.addEventListener(`load`, onLoad)
+            return () => script.removeEventListener(`load`, onLoad)
+        } else onLoad()
+    })
+
+    return (
+        <
+            div
+            {...props}
+            style
+                = {{height: `100vh`, }}
+        />
+    )
+}
+
+Map.defaultProps = {
+    options: {
+        center: { lat: 48, lng: 8 },
+        zoom: 5,
+        styles: mapStyles
+    },
+
+
+}
