5840fcdc52241a917bfcb9e3c12629959c47c96d
ashminbhandari <abhandar@ramapo.edu>
2020-09-02 23:24:26 -0500

    #needsfix two maps in one page error

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import MapPage from "./pages/MapPage";
 
 function App() {
-  return (
-    <div className="App">
-      <MapPage/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <MapPage/>
+        </div>
+    );
 }
 
 export default App;
